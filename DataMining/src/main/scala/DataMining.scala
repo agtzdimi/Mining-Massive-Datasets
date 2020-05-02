703ed12dad411c170b655c28b466c7dcd7c16112
Dimitris Agtzidis <agtzdimi@gmail.com>
2020-05-02 13:54:39 +0300

    Commit K means implemenation

--- DataMining/src/main/scala/DataMining.scala
+++ DataMining/src/main/scala/DataMining.scala
@@ -1,9 +1,10 @@
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.ml.feature.{Normalizer, VectorAssembler}
+import org.apache.spark.sql.functions.udf
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.ml.feature.Normalizer
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.sql.functions.{udf, _}
+import org.apache.spark.{SparkConf, SparkContext}
+import UnsupervisedModels.KMeans
+import org.apache.spark.ml.feature.MinMaxScaler
 
 
 object DataMining {
@@ -27,34 +28,45 @@ object DataMining {
     import ss.implicits._ // For implicit conversions like converting RDDs to DataFrames
 
     val inputElements = "src/main/resources/sample_input.txt"
-    val basicTrainDF: DataFrame = ss.read.options(Map("delimiter"->",","header"->"false"))
-     .csv(inputElements)
+    val basicTrainDF: DataFrame = ss.read.options(Map("delimiter" -> ",", "header" -> "false"))
+      .csv(inputElements)
 
     val udf_toDouble = udf((s: String) => s.toDouble)
-    val castedDF = basicTrainDF.select(udf_toDouble($"_c0"),udf_toDouble($"_c1"))
+    val castedDF = basicTrainDF.select(udf_toDouble($"_c0"), udf_toDouble($"_c1"))
 
     print(basicTrainDF.count())
 
     val filteredDF = castedDF.filter(
       ($"_c0" =!= ""))
-        .filter(
-          ($"_c1" =!= ""))
+      .filter(
+        ($"_c1" =!= ""))
       .toDF("el1", "el2")
 
     print(filteredDF.show())
 
-    val assembler = new VectorAssembler()
-      .setInputCols(Array("el1", "el2"))
-      .setOutputCol("features")
+    var assembler = new VectorAssembler()
+      .setInputCols(Array("el1"))
+      .setOutputCol("el1Features")
 
-    val output = assembler.transform(filteredDF)
+    var output = assembler.transform(filteredDF)
 
-    val normalizer = new Normalizer()
-      .setInputCol("features")
-      .setOutputCol("normalizedFeatures")
-      .setP(1.0)
+    var normalizer = new MinMaxScaler()
+      .setInputCol("el1Features")
+      .setOutputCol("normalizedEl1Features")
 
-    val l1NormData = normalizer.transform(output)
+    var l1NormData = normalizer.fit(output).transform(output)
+
+    assembler = new VectorAssembler()
+      .setInputCols(Array("el2"))
+      .setOutputCol("el2Features")
+
+    output = assembler.transform(l1NormData)
+
+    normalizer = new MinMaxScaler()
+      .setInputCol("el2Features")
+      .setOutputCol("normalizedEl2Features")
+
+    l1NormData = normalizer.fit(output).transform(output)
 
     println("Normalized using L^1 norm")
     l1NormData.show(false)
@@ -63,6 +75,9 @@ object DataMining {
     /* =======================KMeans =========================== */
     /* ======================================================= */
 
+    val combinedHotness = new KMeans(sc,l1NormData.select("normalizedEl1Features").rdd.map(_.get(0).toString().replace("[","").replace("]","").toDouble), l1NormData.select("normalizedEl2Features").rdd.map(_.get(0).toString().replace("[","").replace("]","").toDouble),5, 10)
+
+    combinedHotness.run( "AITEEEEEEE.txt")
     sc.stop()
   }
 
